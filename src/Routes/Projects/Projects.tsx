828866955ce82fb94673447a8f04e4b9c36abdf7
gusehd66 <rlagusehd45@naver.com>
2022-03-22 14:57:06 +0900

    :fire: 안쓰는 코드 제거

--- src/Routes/Projects/Projects.tsx
+++ src/Routes/Projects/Projects.tsx
@@ -1,20 +1,20 @@
 import { RefObject, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Outlet, useParams } from "react-router-dom";
 import styled from "styled-components";
 import Container from "../../component/Common/Container";
 import { projectActions } from "../../redux/store/project_reducer";
-import { RootState } from "../../redux/store/store";
 import ProjectMain from "./ProjectMain/ProjectMain";
 
-const ProjectsContainer = styled(Container)``;
+const ProjectsContainer = styled(Container)`
+  justify-content: center;
+`;
 
 interface Props {
   nodeRef: RefObject<HTMLInputElement>;
 }
 
 const Projects = ({ nodeRef }: Props) => {
-  const { param } = useSelector((state: RootState) => state.project);
   const { id } = useParams();
   const dispatch = useDispatch();
 
